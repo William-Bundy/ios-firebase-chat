11b65856ecf410cd7fe5efebc7da3c49ebd835ab
William Bundy <williambundy@icloud.com>
2018-09-18 14:05:41 -0700

    Initial Commit

---
+++ EmberChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmberChat
+//
+//  Created by William Bundy on 9/18/18.
+//  Copyright © 2018 William Bundy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
